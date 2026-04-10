96a06532258789e281daaac1e63c143a8cdbd471
Eduardo <leeeduardo924@gmail.com>
2026-04-10 12:26:41 -0400

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
-//passwords are "1111"
+--passwords are "1111"
 
 INSERT INTO sec_user (email, encryptedPassword, enabled)
-VALUES ('worker@warehouse.ca', '$2a$12$OnXRsTFKZcqhnOu3zoUL8.pQMec8GPuxU6ZETsyhDOqrlbA4bLuhG', 1);
+VALUES ('worker@warehouse.ca', '$2a$12$88UzWYqtLsl3918w7qCiYOJ/woshfWQjdYStSsgiXHcwCa07KpTXS', 1);
 
 INSERT INTO sec_user (email, encryptedPassword, enabled)
-VALUES ('manager@warehouse.ca', '$2a$12$OnXRsTFKZcqhnOu3zoUL8.pQMec8GPuxU6ZETsyhDOqrlbA4bLuhG', 1);
+VALUES ('manager@warehouse.ca', '$2a$12$88UzWYqtLsl3918w7qCiYOJ/woshfWQjdYStSsgiXHcwCa07KpTXS', 1);
 
 INSERT INTO sec_role (roleName) VALUES ('ROLE_STAFF');
 INSERT INTO sec_role (roleName) VALUES ('ROLE_MANAGER');
